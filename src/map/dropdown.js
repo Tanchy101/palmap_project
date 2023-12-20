f58c9adfbbcaba575d420002399071be6fe5ce32
rimuelcanada14 <102201493+rimuelcanada14@users.noreply.github.com>
2023-12-20 17:19:57 +0800

    button for my profile

--- src/map/dropdown.js
+++ src/map/dropdown.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 
 function DropDown(){
     return(
         <>
             <div className="drpmn">
-                <button>Profile</button>
+            <Link to ={"/myProfile"} className = "myProfile"><button>Profile</button></Link>
                 <button>About</button>
                 <button>Logout</button>
             </div>
